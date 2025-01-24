a72a9bf7175b071d06ab2ea202d389716d45f6ed
Michał <michalOzdz@icloud.com>
2025-01-25 00:28:27 +0100

    Update word list in generateAns.php for game logic

--- scripts/php/generateAns.php
+++ scripts/php/generateAns.php
@@ -1,13 +1,13 @@
 <?php /** @noinspection ALL */
-const SLOWA = array(3 => array("kot", "dom", "las", "miś", "dąb"),
-    4 => array("ryba", "pies", "kawa", "mama", "słoń", "stół", "tata", "lato", "zima", "ptak", "koza", "wino", "okno"),
-    5 => array("rower", "ogród", "drzwi", "pomoc", "kwiat", "lampa", "łóżko", "piłka"),
-    6 => array("apteka", "jabłko", "szkoła", "drzewo", "wiosna", "świnia", "ogórek", "słońce", "cukier", "kanapa", "kwiaty"),
-    7 => array("fryzjer", "samolot", "krzesło", "traktor", "telefon", "fotelik", "rodzina", "podróże", "książka", "tęczowy"),
-    8 => array("człowiek", "śniegowy", "tygodnik", "komputer", "szklanka", "kwiatowy", "kuchnia", "ceramika", "jedzenie", "sierpień"),
-    9 => array("samochody", "telewizor", "kalendarz", "słoneczny", "pracowity", "truskawka", "dzieciaki", "dziecięcy", "nauczyciel", "gospodarz",),
-    10 => array("ceramiczna", "miesiączka", "mieszkanie", "biblioteka", "księgarnia", "niedzielny", "przyjaciel"),
-    11 => array("przedszkole", "przedszkola", "miesięcznik", "telewizyjny", "komputerowy"));
+const SLOWA = array(3 => array("kot", "dom", "las", "kij"),
+    4 => array("ryba", "pies", "kawa", "mama", "tata", "lato", "zima", "ptak", "koza", "wino", "okno"),
+    5 => array("rower", "drzwi", "pomoc", "kwiat", "lampa"),
+    6 => array("apteka",  "drzewo", "wiosna", "cukier", "kanapa", "kwiaty"),
+    7 => array("fryzjer", "samolot", "traktor", "telefon", "fotelik", "rodzina"),
+    8 => array("tygodnik", "komputer", "szklanka", "kwiatowy", "kuchnia", "ceramika", "jedzenie"),
+    9 => array("samochody", "telewizor", "kalendarz", "pracowity", "truskawka", "dzieciaki", "nauczyciel", "gospodarz",),
+    10 => array("ceramiczna", "mieszkanie", "biblioteka", "niedzielny", "przyjaciel"),
+    11 => array("przedszkole", "przedszkola", "telewizyjny", "komputerowy"));
 
 function generateAns($wordLength): string
 {
